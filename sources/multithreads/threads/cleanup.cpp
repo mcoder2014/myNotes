aee2998b00d40e03e68880bcb1d375d36e257113
mcoder <mcoder2014@sina.com>
2019-12-29 21:14:03 +0800

    apue

--- sources/multithreads/threads/cleanup.cpp
+++ sources/multithreads/threads/cleanup.cpp
@@ -11,8 +11,8 @@ void *
 thr_fn1(void *arg)
 {
 	printf("thread 1 start\n");
-	pthread_cleanup_push(cleanup, "thread 1 first handler");
-	pthread_cleanup_push(cleanup, "thread 1 second handler");
+	pthread_cleanup_push(cleanup, const_cast<char*>("thread 1 first handler"));
+	pthread_cleanup_push(cleanup, const_cast<char*>("thread 1 second handler"));
 	printf("thread 1 push complete\n");
 	if (arg)
 		return((void *)1);
@@ -25,8 +25,8 @@ void *
 thr_fn2(void *arg)
 {
 	printf("thread 2 start\n");
-	pthread_cleanup_push(cleanup, "thread 2 first handler");
-	pthread_cleanup_push(cleanup, "thread 2 second handler");
+	pthread_cleanup_push(cleanup, const_cast<char *>("thread 2 first handler"));
+	pthread_cleanup_push(cleanup, const_cast<char *>("thread 2 second handler"));
 	printf("thread 2 push complete\n");
 	if (arg)
 		pthread_exit((void *)2);
