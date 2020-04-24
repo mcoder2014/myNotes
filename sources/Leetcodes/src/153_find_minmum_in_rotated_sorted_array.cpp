78656c9d67c4b9c171015983cfc1192cfeda8f71
Chaoqun <jiangchaoqun@bytedance.com>
2020-04-24 13:57:22 +0800

    LeetCode 找到旋转数组最小值

---
+++ sources/Leetcodes/src/153_find_minmum_in_rotated_sorted_array.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int len = nums.size();
+        int left = 0 ;
+        int right = len-1;
+        while(left < right){
+            int mid = (right + left)/2;
+
+            if(nums[mid] < nums[right]){
+                right = mid;
+            }else if(nums[mid] > nums[right]){
+                left = mid+1;
+            }else if(nums[mid] == nums[right]){
+                if(nums[right] == nums[right-1]){
+                    right--;
+                }else{
+                    left++;
+                }
+            }
+        }
+        return nums[left];
+    }
+};
